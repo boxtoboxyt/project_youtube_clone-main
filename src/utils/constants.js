391b9a269dceee0a669bcfe6e30d6e16bdec0501
Romain Gaudel <romaingaudel83@gmail.com>
2023-01-12 15:24:08 +0100

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,42 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'Gigawatts', icon: <CodeIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'ReactJS', icon: <CodeIcon />, },
+  { name: 'NextJS', icon: <CodeIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://yt3.ggpht.com/2aej0bmyJtcZ93mdpAhlHYBM3qU12I3VgczgSIfddgbHNMeufMGc3bj_u7hOUwdvxTktTtM9=s600-c-k-c0x00ffffff-no-rj-rp-mo';
+export const demoChannelUrl = '/channel/@Gigawatts';
+export const demoVideoUrl = '/video/c69b3bv0uXE&';
+export const demoChannelTitle = 'Gigawatts';
+export const demoVideoTitle = 'Gorr Le Boucher des dieux - Gigawatts';
+export const demoProfilePicture = 'https://yt3.ggpht.com/2aej0bmyJtcZ93mdpAhlHYBM3qU12I3VgczgSIfddgbHNMeufMGc3bj_u7hOUwdvxTktTtM9=s600-c-k-c0x00ffffff-no-rj-rp-mo'
